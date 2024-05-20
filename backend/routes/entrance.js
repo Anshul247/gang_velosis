35d6ef5be37a301ad2e48724926f6f50084bf635
Anshul247 <abhiudai04singh@gmail.com>
2024-05-20 15:46:02 +0530

    "get all users demo api"

--- backend/routes/entrance.js
+++ backend/routes/entrance.js
@@ -13,5 +13,6 @@ const entranceController = require('../controllers/entrance/index');
 
 
 router.post('/get-otp', entranceController.getOtp);
+router.post('/get-all-users', entranceController.getAllUsers);
 router.post('/verify-otp', entranceController.verifyOtp);
 module.exports = router;
