d8d988537fb2b5f83b9f16b90f9b4c6b43026b01
Anshul247 <abhiudai04singh@gmail.com>
2024-05-25 16:14:00 +0530

    Shutdown API's

--- backend/controllers/complain/update-request-shutdown.js
+++ backend/controllers/complain/update-request-shutdown.js
@@ -4,8 +4,62 @@ const ShutDownModel = require("../../models/ShutDown");
 const ComplainModel = require("../../models/Complain");
 const StatusModel = require("../../models/Status");
 
-const updateRequestShutdown = async (req, res) => {
+// const updateRequestShutdown = async (req, res) => {
     
+//     const {
+//         complain_no,
+//         statusId,
+//         staff_remark,
+//         remark,
+//         site_pic,
+//         current_sit_pic,
+//         equipment_pic,
+//         latitude,
+//         longitude,
+//         start_time,
+//         end_time,
+//     } = req.body;
+
+    
+//     if (!complain_no) {
+//         return res.status(400).json({ message: 'ComplainNo is required', status: 400 });
+//     }
+
+//     try {
+//         const allStatus = await StatusModel.find();
+//         let foundStatus = allStatus.find(status => status.status_id == statusId);
+//         let statusIdFound = foundStatus ? foundStatus._id : null;
+
+      
+
+//      let complaintDetails =   await ComplainModel.findOneAndUpdate(
+//             { complain_no: complain_no },
+//             { $set: { status: statusIdFound } }
+//         );
+
+
+//         console.log("complaintDet2345ails ",complaintDetails);
+
+
+//       let gangWorkDetails =   await ShutDownModel.findOneAndUpdate(
+//             { _id: complaintDetails.gang_id },
+//             { $set: { staff_remark: staff_remark, remark: remark,site_pic:site_pic,current_sit_pic:current_sit_pic,equipment_pic:equipment_pic,latitude:latitude,longitude:longitude,start_time:start_time,end_time:end_time } }
+//         );
+
+//         if (!gangWorkDetails) {
+//             return res.status(400).json({ message: 'Gang work details not found', status: 400 });
+//         }
+
+//         console.log("ComplainModel complaintDetails",complaintDetails.gang_id );
+
+//         res.status(200).json({ message: 'Update successful', status: 200 });
+//     } catch (error) {
+//         console.error('Error updating shutdown request:', error);
+//         return res.status(500).json({ message: 'Update Failed', status: 500 });
+//     }
+// };
+
+const updateRequestShutdown = async (req, res) => {
     const {
         complain_no,
         statusId,
@@ -20,40 +74,61 @@ const updateRequestShutdown = async (req, res) => {
         end_time,
     } = req.body;
 
-    // Check if id and complain_no are provided
+    // Check if complain_no is provided
     if (!complain_no) {
         return res.status(400).json({ message: 'ComplainNo is required', status: 400 });
     }
 
     try {
         const allStatus = await StatusModel.find();
-        let foundStatus = allStatus.find(status => status.status_id == statusId);
-        let statusIdFound = foundStatus ? foundStatus._id : null;
+        const foundStatus = allStatus.find(status => status.status_id == statusId);
+        const statusIdFound = foundStatus ? foundStatus._id : null;
 
-      
+        if (!statusIdFound) {
+            return res.status(400).json({ message: 'Invalid status ID', status: 400 });
+        }
 
-     let complaintDetails =   await ComplainModel.findOneAndUpdate(
+        const complaintDetails = await ComplainModel.findOneAndUpdate(
             { complain_no: complain_no },
-            { $set: { status: statusIdFound } }
+            { $set: { status: statusIdFound } },
+            { new: true }  // Return the updated document
         );
 
-      let gangWorkDetails =   await ShutDownModel.findOneAndUpdate(
+        if (!complaintDetails) {
+            return res.status(400).json({ message: 'Complaint No is invalid', status: 400 });
+        }
+
+        const gangWorkDetails = await ShutDownModel.findOneAndUpdate(
             { _id: complaintDetails.gang_id },
-            { $set: { staff_remark: staff_remark, remark: remark,site_pic:site_pic,current_sit_pic:current_sit_pic,equipment_pic:equipment_pic,latitude:latitude,longitude:longitude,start_time:start_time,end_time:end_time } }
+            {
+                $set: {
+                    staff_remark,
+                    remark,
+                    site_pic,
+                    current_sit_pic,
+                    equipment_pic,
+                    latitude,
+                    longitude,
+                    start_time,
+                    end_time
+                }
+            },
+           
         );
 
         if (!gangWorkDetails) {
             return res.status(400).json({ message: 'Gang work details not found', status: 400 });
         }
 
-        console.log("ComplainModel complaintDetails",complaintDetails.gang_id );
-
         res.status(200).json({ message: 'Update successful', status: 200 });
     } catch (error) {
         console.error('Error updating shutdown request:', error);
-        return res.status(500).json({ message: 'Update Failed', status: 500 });
+        res.status(500).json(           
+            {error: err.message, message: 'Error while updating data in DB.',status:500 }
+        );
     }
 };
+
 
 module.exports = updateRequestShutdown;
 
